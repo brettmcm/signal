805a2a573a3265bb03c7916af4fd11379d065c8d
Brett McMillin <brettmcm@me.com>
2026-08-14 23:20:21 -0700

    added Nearterm signal

---
+++ src/capsules/006/01/meta.ts
@@ -0,0 +1,8 @@
+import type { CapsuleMetadata } from "../../../lib/capsules";
+
+export const metadata = {
+  signalNumber: "006",
+  capsuleNumber: "01",
+  signalSlug: "disposable-apps-durable-data",
+  title: "Nearterm",
+} satisfies CapsuleMetadata;
